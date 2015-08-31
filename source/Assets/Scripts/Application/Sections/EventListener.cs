4a3ed052cf745fc61224f88f0a4a6f68aaafadb5
Tali Kaniatobe <tkaniatobe@gmail.com>
2015-08-30 22:00:34 -0700

    cleanup

---
+++ source/Assets/Scripts/Application/Sections/EventListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventListener : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnEnable() {
+		EventManager.OnPoweredUp += OnPoweredUp;
+	}
+
+	void OnDisalbe() { 
+		EventManager.OnPoweredUp -= OnPoweredUp;
+	}
+
+	private void OnPoweredUp( bool IsPoweredUp, int Value) { 
+		Debug.Log( IsPoweredUp);
+		Debug.Log( Value );	
+	}
+
+}
---
+++ source/Assets/Scripts/Application/Sections/EventManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+	public delegate void PowerUpHandler( bool IsPoweredUp, int Value );
+	public static event PowerUpHandler OnPoweredUp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnGUI() {
+		if( GUI.Button( new Rect(5,5,150,40), "Power Up")) {
+			if(OnPoweredUp != null) {
+				OnPoweredUp(true, 3);
+			}
+		}
+
+		if( GUI.Button( new Rect(5,50,150,40), "Power Up Over")) {
+			if(OnPoweredUp != null) {
+				OnPoweredUp(false, 4);
+			}
+		}
+
+	}
+
+}
